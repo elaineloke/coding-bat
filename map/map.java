217c3dfc60c069f6ecb74cffd14974bdad9a626c
Elaine Loke <89115854+elaineloke@users.noreply.github.com>
2022-02-13 23:22:20 +0000

    Update map.java

--- map/map.java
+++ map/map.java
@@ -1,5 +1,6 @@
 
 //Modify and return the given map as follows: if the keys "a" and "b" have values that have different lengths, then set "c" to have the longer value. If the values exist and have the same length, change them both to the empty string in the map.
+
 public Map<String, String> mapAB4(Map<String, String> map) {
   if(map.containsKey("a") && map.containsKey("b")){
   if(map.get("a").length() > map.get("b").length()){
