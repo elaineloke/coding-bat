88265489fa1f7064ac887d86567a92deafe5cae8
“elaineloke” <lokeelaine@gmail.com>
2022-02-13 23:03:27 +0000

    Added more exercises

--- warmUps/warmups.java
+++ warmUps/warmups.java
@@ -565,3 +565,19 @@ public String everyNth(String str, int n) {
 	}
 	return result;
 	}
+
+
+//	Given a string and a non-negative int n, return a larger string that is n copies of the original string.
+//
+//
+//	stringTimes("Hi", 2) → "HiHi"
+//	stringTimes("Hi", 3) → "HiHiHi"
+//	stringTimes("Hi", 1) → "Hi"
+
+public String stringTimes(String str, int n) {
+	String result = "";
+	for(int i=0; i<n; i++){
+	result = result + str;
+	}
+	return result;
+	}
