dfb73c53e27180b338799c7ae844e812b64e8757
“elaineloke” <lokeelaine@gmail.com>
2022-02-13 23:08:09 +0000

    new edits

--- warmUps/warmups.java
+++ warmUps/warmups.java
@@ -581,3 +581,27 @@ public String stringTimes(String str, int n) {
 	}
 	return result;
 	}
+
+
+
+//	Given a string and a non-negative int n, we'll say that the front of the string is the first 3 chars, or whatever is there if the string is less than length 3. Return n copies of the front;
+//
+//
+//	frontTimes("Chocolate", 2) → "ChoCho"
+//	frontTimes("Chocolate", 3) → "ChoChoCho"
+//	frontTimes("Abc", 3) → "AbcAbcAbc"
+
+public String frontTimes(String str, int n) {
+	String head = "";
+	if(str.length() > 3) {
+	head = str.substring(0,3);
+	} else {
+	head = str;
+	}
+
+	String result = "";
+	for(int i=0; i < n; i++){
+	result = result + head;
+	}
+	return result;
+	}
